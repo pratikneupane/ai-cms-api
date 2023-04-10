cea4ea951908a3e885d9e77cebb5693843566794
Pratik Neupane <pratikneupane1999@gmail.com>
2023-04-11 03:22:00 +0545

    Revert "chore: added .env.example file, so i won't get lost in the future"
    This reverts commit d6fde54f9468e95b0721160922e765bf6920301f.

--- index.ts
+++ index.ts
@@ -1,8 +1,15 @@
+import { Configuration, OpenAIApi } from "openai";
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
-import { gptService } from "./services/gptService";
+import dotenv from "dotenv";
 
-export type Message = {
+dotenv.config();
+
+const token = process.env.API_TOKEN!;
+const config = new Configuration({ apiKey: token });
+const openai = new OpenAIApi(config);
+
+interface Message {
   role: string;
   content: string;
 }
@@ -18,22 +25,50 @@ app.post("/write", (req: Request, res: Response) => {
     { role: "system", content: style },
     { role: "user", content: message }
   );
-  gptService(res, messages);
+  gptService(res);
 });
 
-// app.post("/image", (req: Request, res: Response) => {
-//   const { prompt } = req.body;
-//   openai
-//     .createImage({
-//       prompt,
-//       n: 1,
-//       size: "1024x1024",
-//     })
-//     .then((data) => {
-//       res.send(data.data.data[0].url);
-//     })
-//     .catch((err) => console.log(err));
-// });
+app.post("/image", (req: Request, res: Response) => {
+  const { prompt } = req.body;
+  openai
+    .createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+    })
+    .then((data) => {
+      res.send(data.data.data[0].url);
+    })
+    .catch((err) => console.log(err));
+});
+
+const gptService = (res: Response) => {
+  openai
+    .createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages,
+    })
+    .then((data) => {
+      messages.push({
+        role: "assistant",
+        content: data.data.choices[0].message!.content,
+      });
+      if (data.data.choices[0].finish_reason !== "stop") {
+        messages.push({
+          role: "user",
+          content: "continue",
+        });
+        gptService(res);
+      } else {
+        res.send(
+          messages
+            .filter((message) => message.role === "assistant")
+            .map((message) => message.content)
+        );
+      }
+    })
+    .catch((err) => console.log(err));
+};
 
 app.listen(9000, () => {
   console.log("Server Running at Port 9000 🚀");
